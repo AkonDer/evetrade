24e61e5b5361f5a49868041e923eb0f7602339ce
Ivan <van.koshkin.fr@gmail.com>
2023-11-28 17:32:05 +0300

    some commit

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import os
+import time
+import threading
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def scan_directory(directory, known_files):
+    try:
+        current_files = set(os.listdir(directory))
+        new_files = current_files - known_files
+        if new_files:
+            print("Обнаружены новые файлы:")
+            for file in new_files:
+                print(file)
+        return current_files
+    except FileNotFoundError:
+        print(f"Директория {directory} не найдена.")
+        return known_files
+    except PermissionError:
+        print(f"Нет разрешения на доступ к директории {directory}.")
+        return known_files
 
 
+def directory_monitor(directory):
+    known_files = set()
+    while True:
+        known_files = scan_directory(directory, known_files)
+        time.sleep(1)  # Пауза в 5 секунд между проверками
+
+
+def start_monitoring_thread(directory_path):
+    monitor_thread = threading.Thread(target=directory_monitor, args=(directory_path,), daemon=True)
+    monitor_thread.start()
+    return monitor_thread
+
+
+# Путь к директории
+directory_path = "C:/Users/Admin/Documents/EVE/logs/Marketlogs"
+
+# Запуск мониторинга в отдельном потоке
+monitor_thread = start_monitoring_thread(directory_path)
+
+# Основной поток программы может продолжать выполнять другие задачи
+try:
+    while True:
+        time.sleep(1)  # Для примера, основной поток просто ждёт
+except KeyboardInterrupt:
+    print("Программа остановлена пользователем.")
---
+++ models.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Float, Boolean, DateTime, String, Date
+
+# SQLAlchemy Model
+Base = declarative_base()
+
+
+class MarketLog(Base):
+    __tablename__ = 'market_logs'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    recordDate = Column(Date)
+    price = Column(Float)
+    volRemaining = Column(Integer)
+    typeID = Column(Integer)
+    range = Column(String)
+    orderID = Column(Integer)
+    volEntered = Column(Integer)
+    minVolume = Column(Integer)
+    bid = Column(Boolean)
+    issueDate = Column(DateTime)
+    duration = Column(Integer)
+    stationID = Column(Integer)
+    regionID = Column(Integer)
+    solarSystemID = Column(Integer)
+    jumps = Column(Integer)
+
+
+def read_file_to_db(file_path, db_url='sqlite:///evetrade_data.db'):
+    # Create engine and session
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Read file using pandas
+    df = pd.read_csv(file_path)
+
+    # Convert DataFrame to list of MarketLog objects
+    records = df.to_dict(orient='records')
+    market_logs = [MarketLog(**record) for record in records]
+
+    # Add records to database session and commit
+    session.add_all(market_logs)
+    session.commit()
+
+    # Close the session
+    session.close()
+
+
+# Replace with your actual file path
+file_path = 'C:/Users/Admin/Documents/EVE/logs/Marketlogs/The Forge-Small Energy Collision Accelerator II-2023.11.28 094356.txt'
+
+# Call the function
+read_file_to_db(file_path)
